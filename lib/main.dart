edf88a579a611fe6982128219d2d5e37bd7d3d63
Clebert Figueiredo <clebert.figueiredo@otimizeit.com.br>
2020-08-23 16:04:35 -0300

    Finished project 🚀

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_de_gifs/ui/home_page.dart';
+import 'package:buscador_de_gifs/ui/gif_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        )),
+  ));
+}
